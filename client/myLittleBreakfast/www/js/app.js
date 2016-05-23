d95437046fbe3628c2a499a5777cbcad44022963
PttBiscuit <christelle.webdev@gmail.com>
2016-05-23 09:21:54 +0200

    Adding feature shopping cart - NgCart directives

--- client/myLittleBreakfast/www/js/app.js
+++ client/myLittleBreakfast/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('mlb', ['ionic', 'mlb.controllers', 'mlb.services'])
+angular.module('mlb', ['ionic', 'ngCart', 'mlb.controllers', 'mlb.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
---
+++ removeItemFormCart.js
@@ -0,0 +1,7 @@
+db.getCollection('shoppingcarts').find({})
+
+db.shoppingcarts.update(
+  { "_id" : ObjectId("573f1804ff28e6584245f518")},
+  { $pull: { items : { price: "6.00" } } },
+  { multi: true }
+)
